b472164e177331c2bd3412afe57521828ed8c716
papunag209 <papunag209@gmail.com>
2016-06-06 23:07:26 +0400

    Message
    added message class

---
+++ src/backend/Message.java
@@ -0,0 +1,50 @@
+package backend;
+
+public class Message {
+	private String text;
+	private String subject;
+	
+	private User sender;
+	private User recipient;
+	
+	Message(String text, String subject, User sender, User recipient){
+		this.text = text;
+		this.subject = subject;
+		this.sender = sender;
+		this.recipient = recipient;
+	}
+	
+	public String getText(){
+		return text;
+	}
+	
+	public String getSubject(){
+		return subject;
+	}
+	
+	public User getSender(){
+		return sender;
+	}
+	
+	public User getRecipient(){
+		return recipient;
+	}
+	
+	public String getSenderName(){
+		return sender.getName();
+	}
+
+	public String getRecipientName(){
+		return recipient.getName();
+	}
+	
+	public Integer getSenderID(){
+		return sender.getId();
+	}
+
+	public Integer getRecipientID(){
+		return recipient.getId();
+	}
+
+
+}
