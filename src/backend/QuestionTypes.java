d7d527c2bf3f8e11649c6b872622937a5e7f2a79
papunag209 <papunag209@gmail.com>
2016-06-06 18:52:25 +0400

    Question Types
    added constants for easy matching of question types

---
+++ src/backend/QuestionTypes.java
@@ -0,0 +1,14 @@
+package backend;
+
+public class QuestionTypes {
+	
+	public static final String QuestionResponse = "QuestionResponse";
+	public static final String FillInTheBlanks = "FillInTheBlanks";
+	public static final String MultipleChoice = "MultipleChoice";
+	public static final String PictureResponse = "PictureResponse";
+	public static final String MultiAnswer = "MultiAnswer";
+	public static final String MultipleChoiceMultipleAnswer = "MultipleChoiceMultipleAnswer";
+	public static final String Matching = "Matching";
+		
+
+}
