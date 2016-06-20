6a2dc8c0304f9db4103ab1dc33f69f594cd5d2c1
papunag209 <papunag209@gmail.com>
2016-06-20 14:56:16 +0400

    added instance variable
    added instance variable ad getter for numAnswers.

--- src/questions/QuestionWithMultipleAnswers.java
+++ src/questions/QuestionWithMultipleAnswers.java
@@ -9,15 +9,21 @@ import answers.AnswerParent;
 
 public class QuestionWithMultipleAnswers extends QuestionParent{
 	private boolean order;
+	private int numAnswers;
 	public QuestionWithMultipleAnswers(String question_text, String question_type, String question_description,
-			long question_time_limit, int question_score, boolean order, ArrayList<AnswerParent> answers){
+			long question_time_limit, int question_score, int numAnswers, boolean order, ArrayList<AnswerParent> answers){
 		super(question_text, question_type, question_description, question_time_limit, question_score, answers);
 		this.order=order;
+		this.numAnswers = numAnswers;
 	}
 	
 	public String toString(){
 		String result = super.toString()+"order: "+order+"\n";
 		return result;
+	}
+	
+	public int getNumanswers(){
+		return numAnswers;
 	}
 	
 }
