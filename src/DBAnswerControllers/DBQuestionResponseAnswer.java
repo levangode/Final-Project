7e338b3622a21909c50171141b75cdd974e02d74
papunag209 <papunag209@gmail.com>
2016-06-24 21:15:14 +0400

    z
    z

--- src/DBAnswerControllers/DBQuestionResponseAnswer.java
+++ src/DBAnswerControllers/DBQuestionResponseAnswer.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import answers.ResponseAnswer;
 import database.DBconnector;
 import questions.QuestionTypes;
 import questions.QuestionWithMultipleAnswers;
@@ -24,11 +25,11 @@ public class DBQuestionResponseAnswer {
 		this.connection = con;
 	}
 	
-	public List<QuestionWithMultipleAnswers> retrieveQuestions(int quiz_id){
-		List<QuestionWithMultipleAnswers> questions = new ArrayList<QuestionWithMultipleAnswers>();
+	public List<ResponseAnswer> retrieveAnswers(int answer_id){
+		List<ResponseAnswer> answers = new ArrayList<ResponseAnswer>();
 		
-		String query = "select question_text, question_data, question_time_limit, num_answers, score, answers_ordered from Questions_MultipleAnswers where quiz_id = "
-					+ quiz_id + " ;";
+		String query = "select answer_text from answers_questionresponse where question_id = "
+					+ answer_id + " ;";
 		
 		PreparedStatement stm;
 		
@@ -37,20 +38,9 @@ public class DBQuestionResponseAnswer {
 			ResultSet rs = stm.executeQuery();
 			
 			while(rs.next()){
-				QuestionWithMultipleAnswers newQuestion;
+				ResponseAnswer newAnswer = new ResponseAnswer(rs.getString(1));
 				
-				newQuestion = new QuestionWithMultipleAnswers(
-								rs.getString(1),
-								QuestionTypes.MultiAnswer,
-								rs.getString(2),
-								rs.getLong(3),
-								rs.getInt(4),
-								rs.getInt(5),// this is for numAnswers but it is not in constructor
-								rs.getBoolean(6),
-								null // TODO here should be arraylist of answers
-							);
-				
-				questions.add(newQuestion);
+				answers.add(newAnswer);
 			}
 			
 			connection.close();
@@ -60,39 +50,31 @@ public class DBQuestionResponseAnswer {
 			e.printStackTrace();
 		}
 		
-		return questions;
+		return answers;
 	}
 	
-	public void addQuestion(QuestionWithMultipleAnswers question, int quiz_id) throws Exception{
-		String query = 
-				"insert into Questions_MultipleAnswers(quiz_id, question_text, question_data, question_time_limit, score, num_answers, answers_ordered) value ("
-				+ quiz_id + ", "
-				+ question.getQuestiontext() + ", "
-				+ question.getQuestiondescription() + ", "
-				+ question.getQuestiontimelimit() + ", "
-				+ question.getQuestionscore() + ", "
-				+ question.getNumanswers() + ", "
-				+ question.getAnswersordered()
+	public void addAnswer(ResponseAnswer ans, int question_id){
+		String query = "insert into answers_questionresponse(answer_text, question_id) values("
+				+ ans
+				+ ", "
+				+ question_id
 				+ ");";
-		
 		PreparedStatement stm;
 		
 		try{
-			stm = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-
+			stm = connection.prepareStatement(query);
 			stm.executeQuery();
 			
-			ResultSet rs = stm.getGeneratedKeys();
-			int question_id = rs.getInt("question_id");
-			// TODO insert answers into its table.			
-			
+		
+		
 			connection.close();
 			
 		} catch (SQLException e) {
 			System.out.println("Error occured durin database connection!");
-			
-			throw new Exception("Error durin insertion in DB");
-			//e.printStackTrace();
+			e.printStackTrace();
 		}
+	
 	}
+	
+
 }
