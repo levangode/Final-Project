65203e5d62194841363f8ab419684662c1dc1067
papunag209 <papunag209@gmail.com>
2016-06-26 15:57:50 +0400

    simple tests for database Quiz
    not guaranteed to work because it check if specific quizzes exist

--- Testing/Testing/QuizTest.java
+++ Testing/Testing/QuizTest.java
@@ -1,5 +1,30 @@
 package Testing;
 
-public class QuizTest {
+import org.junit.Test;
 
+import DBQuizControllers.DBQuizController;
+import backend.Quiz;
+
+public class QuizTest {
+	
+	@Test
+	public void QuizTest1(){
+		DBQuizController db = new DBQuizController();
+		
+		Quiz z = db.getQuiz(7);
+		
+		System.out.println(z);
+		//System.out.println(z.getQuiz_name());
+	}
+
+	@Test
+	public void QuizTest2(){
+		DBQuizController db = new DBQuizController();
+		
+		Quiz z = db.getQuiz(9);
+		
+		System.out.println(z);
+		//System.out.println(z.getQuiz_name());
+	}
+	
 }
