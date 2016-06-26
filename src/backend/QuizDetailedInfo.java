a85175878f8eef371ca55b61c0b39a1027c494fe
levangode <lgode14@freeuni.edu.ge>
2016-06-26 18:42:43 +0400

    Sync

--- src/backend/QuizDetailedInfo.java
+++ src/backend/QuizDetailedInfo.java
@@ -24,5 +24,11 @@ public class QuizDetailedInfo extends QuizInfo{
 	public void setQuiz_description(String quiz_description) {
 		this.quiz_description = quiz_description;
 	}
+	public String showOnCard(){
+		String result = "";
+		
+		return result;
+	}
+	
 	
 }
--- src/backend/QuizInfoFactory.java
+++ src/backend/QuizInfoFactory.java
@@ -6,4 +6,7 @@ public class QuizInfoFactory {
 	public static QuizInfo getQuizInfo(String quiz_name, int times_taken, String quiz_author, Timestamp quiz_date, int quiz_id){
 		return new QuizInfo(quiz_name, times_taken, quiz_author, quiz_date, quiz_id);
 	}
+	public static QuizDetailedInfo getDetailedInfo(String quiz_name, int times_taken, String quiz_author, Timestamp quiz_date, int quiz_id,  String quiz_category, String quiz_description){
+		return new QuizDetailedInfo(quiz_name, times_taken, quiz_author, quiz_date, quiz_id, quiz_category, quiz_description);
+	}
 }
