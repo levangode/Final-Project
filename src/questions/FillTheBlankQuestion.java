0e3dde5b5b61ac93e90f3b5ee0632627baf0bf84
mmetr14 <mmetr14@freeuni.edu.ge>
2016-06-27 00:16:42 +0400

    minor tweaking
    a

--- src/questions/FillTheBlankQuestion.java
+++ src/questions/FillTheBlankQuestion.java
@@ -1,6 +1,7 @@
 package questions;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,6 +12,7 @@ import DBQuestionControllers.DBQuestionWithMultipleAnswers;
 
 import answers.Answer;
 import database.DBconnector;
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
 public class FillTheBlankQuestion extends Question {
 
@@ -23,9 +25,10 @@ public class FillTheBlankQuestion extends Question {
 	public String getQuestionHtml(int id) {
 		String html = "";
 		String questionTxt = getQuestiontext();
+		Pattern p = Pattern.compile("____");
 		for (int i = 0; i < getAnswers().size(); i++) {
 			String inputStr = "<input type='text' name='q" + id + "-" + i + "' id='q" + id + "-" + i + "'>";
-			questionTxt = questionTxt.replaceFirst("____", inputStr);
+			questionTxt = questionTxt.replaceFirst(p.pattern(), inputStr);
 		}
 		System.out.println("Generated HTML: " + html);
 		return html;
