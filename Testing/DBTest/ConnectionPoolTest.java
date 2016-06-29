001400e392bcf4dc7fc1e974699c006c7206bbf1
papunag209 <papunag209@gmail.com>
2016-06-29 19:22:17 +0400

    z

--- Testing/DBTest/ConnectionPoolTest.java
+++ Testing/DBTest/ConnectionPoolTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.junit.Test;
 
 import DBConnector.Connector;
+import answers.BlankAnswer;
 import answers.ResponseAnswer;
 
 public class ConnectionPoolTest {
@@ -30,7 +31,7 @@ public class ConnectionPoolTest {
 		List<ResponseAnswer> answers = new ArrayList<ResponseAnswer>();
 		
 		String query = "select answer_text from Answers_QuestionResponse where question_id = "
-					+ 2 + " ;";
+					+ 1 + " ;";
 		
 		PreparedStatement stm;
 		
@@ -54,4 +55,45 @@ public class ConnectionPoolTest {
 		System.out.println(answers);
 		
 	}
+	
+	@Test
+	public void PoolTest2(){
+		Connection con = null;
+		
+		try {
+			con = Connector.getConnection();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		List<BlankAnswer> answers = new ArrayList<BlankAnswer>();
+		
+		String query = "select answer_text, blank_pos from Answers_FillInTheBlanks where question_id = "
+					+ 3 + " ;";
+		
+		PreparedStatement stm;
+		
+		System.out.println(query);
+		
+		try{
+			stm = con.prepareStatement(query);
+			ResultSet rs = stm.executeQuery();
+			
+			while(rs.next()){
+				BlankAnswer newAnswer = new BlankAnswer(rs.getString(1), rs.getInt(2));
+				
+				answers.add(newAnswer);
+			}
+			
+			Connector.returnConnection(con);
+			
+		} catch (SQLException e) {
+			System.out.println("Error occured durin database connection!");
+			e.printStackTrace();
+		}
+		
+		System.out.println(answers);
+		
+	}
+	
 }
